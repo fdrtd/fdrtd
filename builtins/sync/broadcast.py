d69fd48fb42cc0d3b92060c4e984f5aef0914bbc
Ballhausen <ballhausen@bytesforlife.com>
2021-08-31 19:26:37 +0200

    improve code quality (Pylint)

--- builtins/sync/broadcast.py
+++ builtins/sync/broadcast.py
@@ -1,28 +1,35 @@
+"""
+contains microservice Broadcast
+"""
+
 import uuid as _uuid
 
 from fdrtd.server.microservice import Microservice
 
 
 class Broadcast(Microservice):
+    """synchronizes parties through broadcasts"""
 
     def __init__(self, bus, endpoint):
         super().__init__(bus, endpoint)
         self.storage = {}
 
     def create(self, tokens):
+        """creates a new broadcast"""
         uuid = Broadcast._create_deterministic_uuid(tokens)
         return self.callback(uuid)
 
     def send(self, callback, message):
+        """stores a message to be broadcasted"""
         self.storage[callback] = message
-        return None
 
     def receive(self, callback):
+        """retrieves a broadcasted message"""
         return self.storage.get(callback, None)
 
     def delete(self, callback):
+        """deletes a broadcast"""
         del self.storage[callback]
-        return None
 
     @staticmethod
     def _create_deterministic_uuid(tokens):
