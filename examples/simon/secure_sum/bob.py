4b5e96c3467cee06cb8814a072b13b9ef9efdd5c
Ballhausen <ballhausen@bytesforlife.com>
2021-10-24 21:32:14 +0200

    add secure sum example for Simon

---
+++ examples/simon/secure_sum/bob.py
@@ -0,0 +1,46 @@
+"""
+secure sum example with Simon
+
+four fdrtd servers are needed for this example.
+one for each of Alice, Bob, and Charlie.
+and an additional one for invitations and synchronizing.
+the easiest way is to run them all on localhost:
+
+    pip install fdrtd
+    python -m fdrtd.webserver --port=55500 &
+    python -m fdrtd.webserver --port=55501 &
+    python -m fdrtd.webserver --port=55502 &
+    python -m fdrtd.webserver --port=55503 &
+
+then, the three scripts of Alice, Bob, and Charlie may be executed simultaneously.
+"""
+
+
+import representation
+from tools.sync import SyncApi
+
+
+URL_SYNC = "http://localhost:55500"
+URL_ALICE = "http://localhost:55501"
+URL_BOB = "http://localhost:55502"
+URL_CHARLIE = "http://localhost:55503"
+
+SECRET_BOB = 16.0
+NETWORK_BOB = {'nodes': [URL_ALICE, URL_BOB, URL_CHARLIE], 'myself': 1}  # Bob is no. 1 out of 0, 1, 2.
+
+
+if __name__ == "__main__":
+
+    api = representation.Api(URL_BOB)
+    api_sync = SyncApi(URL_SYNC)
+
+    microservice = api.create(protocol="Simon")
+    invitation = api_sync.wait_for_broadcast(["Some", "Shared", "Tokens"])
+    task = microservice.join_task(invitation=invitation, network=NETWORK_BOB)
+    task.input(data=SECRET_BOB)
+    task.start()
+
+    result = None
+    while result is None:
+        result = task.result()
+    print(api.download(result))
