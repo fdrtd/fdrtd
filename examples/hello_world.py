1e2fbebfdf6164627ed94f4fb30f2f50a0f95771
Ballhausen <ballhausen@bytesforlife.com>
2021-10-24 19:35:51 +0200

    edit hello world example

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -1,5 +1,23 @@
+"""
+hello world example
+
+a single fdrtd server is needed for this example.
+the easiest way is to run one on localhost:
+
+    pip install fdrtd
+    python -m fdrtd.webserver --port=55500
+
+then, the following script may be executed.
+"""
+
+
 import representation
 
-api = representation.Api("http://localhost:55500")
-list_of_services = api.list()
-print(list_of_services)
+
+URL = "http://localhost:55500"
+
+
+if __name__ == "__main__":
+    api = representation.Api(URL)
+    list_of_services = api.list()
+    print(list_of_services)
