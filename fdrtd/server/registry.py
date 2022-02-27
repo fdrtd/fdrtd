d4524fb73849dd8feefd1506421ed8dbbc519ce1
Ballhausen <ballhausen@bytesforlife.com>
2022-02-27 21:46:52 +0100

    refactor code

--- fdrtd/server/registry.py
+++ fdrtd/server/registry.py
@@ -9,29 +9,26 @@ class Registry:
         self.root_objects = {}
         fdrtd.server.discovery.discover_builtins_and_plugins(self)
 
-    def register(self, identifiers, item):
+    def register(self, description, item):
         uuid = str(_uuid.uuid4())
-        self.root_objects[uuid] = {'identifiers': identifiers, 'object': item}
+        self.root_objects[uuid] = (description, item)
 
     def list_representations(self):
         """list available server-side objects"""
         result = []
-        for _, microservice in self.root_objects.items():
-            result.append(microservice['identifiers'])
+        for _, (description, item) in self.root_objects.items():
+            result.append(description)
         return result
 
     def get_representation(self, requirements):
 
-        def test(identifiers):
+        for uuid, (description, item) in self.root_objects.items():
             for requirement in requirements:
-                if requirement not in identifiers:
-                    return False
-                if identifiers[requirement] != requirements[requirement]:
-                    return False
-            return True
-
-        for uuid, microservice in self.root_objects.items():
-            if test(microservice['identifiers']):
-                return uuid, microservice['object']
+                if requirement not in description:
+                    break
+                if description[requirement] != requirements[requirement]:
+                    break
+            else:
+                return uuid, item
 
         raise fdrtd.server.exceptions.RootObjectNotFound(requirements)
