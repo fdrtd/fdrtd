ca4a0e80a8b5e7d12bed26b01c33fc380f92e9fd
Hendrik Ballhausen <ballhausen@bytesforlife.com>
2022-07-30 15:50:10 +0200

    update representation source file

--- examples/hello_world/r/hello_world.r
+++ examples/hello_world/r/hello_world.r
@@ -1,5 +1,5 @@
 library(devtools)
-source_url("https://raw.githubusercontent.com/fdrtd/fdrtd/main/fdrtd/clients/r/fdrtd.r")
+source_url("https://raw.githubusercontent.com/bytesforlife/representation/main/r/representation.r")
 
 api <- Api("http://127.0.0.1:55501")
 lst <- api$report()
